8eb7356f57308692233ebb69b715e9fa83a592fa
Vladimir Timofeev <vladimirmail895@gmail.com>
2022-02-01 19:02:19 +0300

    MapKit train: added pointers with description

---
+++ MapKitTest/MapKitTest/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  MapKitTest
+//
+//  Created by Владимир Тимофеев on 30.01.2022.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
